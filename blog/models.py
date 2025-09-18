b009214cb80e3bb518445067a82ad613f649fda2
hellosun-prog <hellosun84@gmail.com>
2025-09-18 12:40:38 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
+
 class Post(models.Model):
-    title = models.CharField(max_length=30) # 문자를 담는 필드, 최대 길이 30
-    content = models.TextField() # 문자열의 길이 제한이 없는 텍스트 필드
+    title = models.CharField(max_length=30)  # 문자를 담는 필드, 최대 길이 30
+    content = models.TextField()  # 문자열의 길이 제한이 없는 텍스트 필드
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     # author: 추후 작성 예정
 
     # pk: 해당 포스트의 pk값
     # title: 해당 포스트의 title값
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self):
+
+        return f'/blog/{self.pk}/'
