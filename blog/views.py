6671b21f1d99568a43dc105a0494fb95667a1323
hellosun-prog <hellosun84@gmail.com>
2025-09-23 11:00:41 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,11 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
     # template_name = 'blog/single_post_page.html'
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 # def single_post_page(request, pk):
 #     post = Post.objects.get(pk=pk)
@@ -37,3 +42,22 @@ class PostDetail(DetailView):
 #             'post': post,
 #         }
 #     )
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': 'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
