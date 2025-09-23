48ee7a151a93f35b5ece3f83106e821c2ae2f780
hellosun-prog <hellosun84@gmail.com>
2025-09-23 15:35:49 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/tests.py
+++ blog/tests.py
@@ -166,3 +166,29 @@ class TestView(TestCase):
             self.assertIn(self.post_001.title, main_area.text)
             self.assertNotIn(self.post_002.title, main_area.text)
             self.assertNotIn(self.post_003.title, main_area.text)
+
+    def test_create_post(self):
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
+
+        self.client.login(username='rumi', password='somepassword')
+
+        response = self.client.get('/blog/create_post/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Create New Post', soup.h1.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Create New Post', main_area.text)
+
+        self.client.post(
+            '/blog/create_post/',
+            {
+                'title': 'Post Form 만들기',
+                'content': "Post Form 페이지를 만듭시다.",
+            }
+        )
+        self.assertEqual(Post.objects.count(), 4)
+        last_post = Post.objects.last()
+        self.assertEqual(last_post.title, "Post Form 만들기")
+        self.assertEqual(last_post.author.username, 'rumi')
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, Category, Tag
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 class PostList(ListView):
     model = Post
     ordering = '-pk'
@@ -46,6 +48,20 @@ class PostDetail(DetailView):
 #         }
 #     )
 
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            response = super(PostCreate, self).form_valid(form)
+            return super(PostCreate, self).form_valid(form)
+
+        else:
+            return redirect('/blog/')
+        
 def category_page(request, slug):
     if slug == 'no_category':
         category = '미분류'
