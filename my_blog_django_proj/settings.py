314d75336eb6ce5d8d11926381ccfde04d88acb7
hellosun-prog <hellosun84@gmail.com>
2025-09-25 15:42:51 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- my_blog_django_proj/settings.py
+++ my_blog_django_proj/settings.py
@@ -145,6 +145,26 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+SOCIALACCOUNT_PROVIDERS = {
+    "google": {
+        "APP": {
+            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+            "secret": os.getenv("GOOGLE_SECRET_KEY"),
+            "key": ""
+        },
+        # These are provider-specific settings that can only be
+        # listed here:
+        "SCOPE": [
+            "profile",
+            "email",
+        ],
+
+        "AUTH_PARAMS": {
+            "access_type": "online",
+        }
+    }
+}
+
 SITE_ID = 1
 
 # ACCOUNT_EMAIL_REQUIRED = True
